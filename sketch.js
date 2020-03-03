e6048cd15b5a531d87e96cb0c94a0ff92c4c22f3
SaummayS <60730894+SaummayS@users.noreply.github.com>
2020-03-03 13:33:26 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,19 @@
 
-// this class describes the properties of a single particleBg.
+// Saummay Singhal
+// ssing47
+// ART 151 Project 2 : Drawing Machine 
+
+// Press X to take a screenshot !!!
+
+//
+// ParticleBg:
+// Class to handle the Particle
+//
 class ParticleBg {
-// setting the co-ordinates, radius and the
-// speed of a particleBg in both the co-ordinates axes.
+
+  //
+  // constructor()
+  //
   constructor(){
     this.x = random(0,width);
     this.y = random(0,height);
@@ -11,9 +22,11 @@ class ParticleBg {
     this.ySpeed = random(-1,1.5);
   }
 
-  
-
-// creation of a particleBg.
+ 
+  //
+  // createParticleBg():
+  // Creates the particle
+  //
   createParticleBg() {
     noStroke();
    var r = random(255); // r is a random number between 0 - 255
@@ -25,7 +38,10 @@ class ParticleBg {
     circle(this.x,this.y,this.r);
   }
 
-// setting the particleBg in motion.
+  // 
+  // moveParticleBg():
+  // sets the particleBg in motion.
+  //
   moveParticleBg() {
     if(this.x < 0 || this.x > width)
       this.xSpeed*=-1;
@@ -35,8 +51,10 @@ class ParticleBg {
     this.y+=this.ySpeed;
   }
 
-// this function creates the connections(lines)
-// between particlesBg which are less than a certain distance apart
+  //
+  // joinParticlesBg():
+  // joins nearby particles
+  //
   joinParticlesBg(paraticlesBg) {
     particlesBg.forEach(element =>{
       let dis = dist(this.x,this.y,element.x,element.y);
@@ -51,119 +69,59 @@ class ParticleBg {
   }
 }
 
-// function Particle(x,y){
-//  this.x = x;
-//  this.y = y;
-
-//  //this.yspeed = 0;
-
-//  this.history = [];
-
-//  this.update = function() {
-//    this.x += random(-15,15);
-//    this.y += random(-15,15);
-//    //this.yspeed += gravity;
-
-//    // if(this.y > height){
-//    //  this.y = height;
-//    //  this.yspeed *= -0.9;
-//    // }
-        
-//         var v = createVector(this.x,this.y);
-//    this.history.push(v);
-//    //println(this.history.length);
-
-//    if (this.history.length > 25){
-//      this.history.splice(0,1);
-//    }
-//  }
-
-//  this.show = function(){
-//    //colorMode(RGB,100,500,10,255);
-//    stroke('#0f0');
-//    //fill(random(255));
-    
-//    ellipse(this.x,this.y,24,24);
-
-//    for(var i = 0;i< this.history.length;i++){
-      
-//      var pos = this.history[i];
-//      ellipse(pos.x,pos.y,8,8);
-//    }
-//  }
-// }
-
-
-// an array to add multiple particlesBg
+// an array to add multiple particles
 let particlesBg = [];
 
-//var particles = [];
-//var gravity = 0.1;
+//
+// mousePressed()
+// adds new particles when mouse is pressed
+//
 function mousePressed(){
   particlesBg.push(new ParticleBg());
-
-  //particles.push(new Particle(300,300));
 }
 
-
+//
+// setup():
+// Creates canvas and initial random particles
+//
 function setup() {
   createCanvas(800, 800);
-
-  // if (particlesBg.length == 0) {
-  //   fill(255);
-  //   textAlign(CENTER);
-  //   textSize(64);
-  //   text("Welcome!!", width / 2, height / 2);
-  // }
-
+  
   //inside setup
 
   for(let i = 0;i<width/10;i++){
     particlesBg.push(new ParticleBg());
-   
-  }
-  //particlesBg.push(new Particle(mouseX,mouseY));
-  //particle = new Particle(100,100);
-
+   }
 }
 
-//let t=0;
+// var to track number of screenshots
 saveCount = 0;
-function draw() {
-  background(0,5);
 
-    // inside draw
+//
+// draw()
+// creates more particles using class ParticleBg
+//
+function draw() {
+ background(0,5);
+   // inside draw
 
 for(let i = 0;i<particlesBg.length;i++) {
   
-  //for(var j = 0; j< particles.length;j++){
-  // if (mouseIsPressed){
- //    particlesBg[i].createParticleBg();
- //    particlesBg[i].moveParticleBg();
- //    particlesBg[i].joinParticlesBg(particlesBg.slice(i));
-    
- // }else{
-  //particles[j].update;
-    //particles[j].show;
     particlesBg[i].createParticleBg();
     particlesBg[i].moveParticleBg();
     particlesBg[i].joinParticlesBg(particlesBg.slice(i));
-  
-   //  particlesBg[i].update;
-    // particlesBg[i].show;
-  // for(var j = 1; j< particles.length;j++){
-  //  particles[j].update;
-  //  particles[j].show;
-  //  }
-    
-    //}
-  //}
+ }
+}
 
-}
-}
+//
+// keyPressed():
+// screenshots on pressing the X key
+//
 function keyPressed() {
   if (key == 'x') {
     save("screenshotART_151_" + saveCount + ".png");
     saveCount++;
   }
 }
+
+// End
